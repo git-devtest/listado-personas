7623f7bccd47b4ba4c6b3d3d16c8ef24580ffc25
Jhon Harold Hincapie Marulanda <89672206+git-devtest@users.noreply.github.com>
2022-12-07 13:58:04 -0500

    Inyeccion Servicio dentro de otro

--- src/app/personas.service.ts
+++ src/app/personas.service.ts
@@ -1,13 +1,18 @@
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./LoggingService.service";
 import { Persona } from "./persona.model";
 
+@Injectable()
 export class PersonasService{
     personas: Persona[] = [
         new Persona('Jhon Harold', 'Hincapie'),
         new Persona('Jeimy', 'Rodriguez'),
     ];
 
+    constructor(private loggingService: LoggingService){}
+
     agregarPersona(persona: Persona) {
+        this.loggingService.enviaMensajeConsola('Hemos agregado a :'+persona._nombre+' al array de personas');
         this.personas.push(persona);
-        //this.loggingService.enviaMensajeConsola('Hemos agregado a :'+persona._nombre+' al array de personas');
     }
 }
